6ea9ec1da9aaad1ae69cf49100abdda9f69c9695
meowpunch <jinhoonbae@gmail.com>
2020-09-02 17:56:42 +0900

    JB: change asset directory

--- constants/Assets.ts
+++ constants/Assets.ts
@@ -1,13 +1,13 @@
 const Image = {
-    Splash: require('../../new-client/assets/images/splash/splash3.jpg'),
-    backgroundImage: require('../../new-client/assets/images/splash/splash.jpg'),
-    logo: require('../../new-client/assets/images/logo/logo.png'),
-    menu: require('../../new-client/assets/images/header/menu.png'),
-    search: require('../../new-client/assets/images/header/search.png'),
-    adduser: require('../../new-client/assets/images/header/adduser.png'),
-    delete: require('../../new-client/assets/images/header/delete.png'),
-    advertise: require('../../new-client/assets/images/advertisement.jpg'),// {uri: 'https://fm-foodpicturebucket.s3.ap-northeast-2.amazonaws.com/frontend/advertisement.jpg'},
-    addItem: require('../../new-client/assets/images/addItem.png')
+    Splash: require('../../client/assets/images/splash/splash3.jpg'),
+    backgroundImage: require('../../client/assets/images/splash/splash.jpg'),
+    logo: require('../../client/assets/images/logo/logo.png'),
+    menu: require('../../client/assets/images/header/menu.png'),
+    search: require('../../client/assets/images/header/search.png'),
+    adduser: require('../../client/assets/images/header/adduser.png'),
+    delete: require('../../client/assets/images/header/delete.png'),
+    advertise: require('../../client/assets/images/advertisement.jpg'),// {uri: 'https://fm-foodpicturebucket.s3.ap-northeast-2.amazonaws.com/frontend/advertisement.jpg'},
+    addItem: require('../../client/assets/images/addItem.png')
 };
 
 const Assets = {
