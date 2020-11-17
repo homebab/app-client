41f66606e4ea44c9a1f7fa35fa7bd07d0bbfd6b2
meowpunch <jinhoonbae@gmail.com>
2020-11-18 02:26:20 +0900

    JB: tidy up cdoe

--- hooks/useEditable.ts
+++ hooks/useEditable.ts
@@ -1,14 +1,19 @@
-import {useReducer, useState} from "react";
+import {Reducer, useReducer, useState} from "react";
+import {Container} from "../contexts/Container";
 
 type Action =
-    | {}
+    | {type: "EXAMPLE"}
 
-function reducer(state: Date, action: Action) {
-
+const reducer: Reducer<Date, Action> = (state: Date, action: Action) => {
+    switch(action.type) {
+        case "EXAMPLE":
+            return state
+    }
 }
 
 export default function useEditable(initialState: Date) {
 
+    const [state, dispatch] = useReducer(reducer, initialState);
     const [editable, setEditable] = useState<boolean>(false);
 
     return
