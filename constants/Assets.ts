214edc297123edaaafc904019395b2e4033a8cf0
meowpunch <jinhoonbae@gmail.com>
2021-02-07 16:46:12 +0900

    JB: translate food name, korean -> english

--- constants/Assets.ts
+++ constants/Assets.ts
@@ -1,52 +1,53 @@
 const Image = {
-    backgroundImage: require('../../app-client/assets/images/splash/splash.jpg'),
-    logo: require('../../app-client/assets/images/logo/home.png'),
+    backgroundImage: require('../assets/images/splash/splash.jpg'),
+    logo: require('../assets/images/logo/home.png'),
 
-    emptyFridge: require('../../app-client/assets/images/emptyFridge.png'),
-    emptyUser: require('../../app-client/assets/images/emptyUser.png'),
+    emptyFridge: require('../assets/images/emptyFridge.png'),
+    emptyUser: require('../assets/images/emptyUser.png'),
 };
 
 const FoodImages = {
     default: require('../assets/images/food/default.png'),
 
     //채소
-    감자: require('../../app-client/assets/images/food/감자.png'),
-    고구마: require('../../app-client/assets/images/food/고구마.png'),
-    토마토: require('../../app-client/assets/images/food/토마토.png'),
-    애호박: require('../../app-client/assets/images/food/애호박.png'),
-    대파: require('../../app-client/assets/images/food/파.png'),
-    버섯: require('../../app-client/assets/images/food/버섯.png'),
-    양파: require('../../app-client/assets/images/food/양파.png'),
-    가지: require('../../app-client/assets/images/food/가지.png'),
-    고추: require('../../app-client/assets/images/food/고추.png'),
-    당근: require('../../app-client/assets/images/food/당근.png'),
+    감자: require('../assets/images/food/potato.png'),
+    고구마: require('../assets/images/food/sweet_potato.png'),
+    토마토: require('../assets/images/food/tomato.png'),
+    양배추: require('../assets/images/food/cabbage.png'),
+    애호박: require('../assets/images/food/squash.png'),
+    대파: require('../assets/images/food/green_onion.png'),
+    버섯: require('../assets/images/food/mushroom.png'),
+    양파: require('../assets/images/food/onion.png'),
+    가지: require('../assets/images/food/eggplant.png'),
+    고추: require('../assets/images/food/pepper.png'),
+    당근: require('../assets/images/food/carrot.png'),
 
     // 과일
-    사과: require('../../app-client/assets/images/food/사과.png'),
-    바나나: require('../../app-client/assets/images/food/바나나.png'),
-    귤: require('../../app-client/assets/images/food/귤.png'),
+    사과: require('../assets/images/food/apple.png'),
+    바나나: require('../assets/images/food/banana.png'),
+    귤: require('../assets/images/food/tangerine.png'),
 
     // 육류
-    돼지: require('../../app-client/assets/images/food/돼지.png'),
-    소고기: require('../../app-client/assets/images/food/소고기.png'),
-    닭: require('../../app-client/assets/images/food/닭.png'),
-    계란: require('../../app-client/assets/images/food/계란.png'),
+    돼지: require('../assets/images/food/pork.png'),
+    소고기: require('../assets/images/food/beef.png'),
+    닭: require('../assets/images/food/chicken.png'),
+    계란: require('../assets/images/food/egg.png'),
 
     // 수산물
-    '고등어,꽁치': require('../../app-client/assets/images/food/고등어.png'),
-    새우: require('../../app-client/assets/images/food/새우.png'),
+    '고등어,꽁치': require('../assets/images/food/mackerel.png'),
+    새우: require('../assets/images/food/shrimp.png'),
 
     // 양념/소스
-    된장: require('../../app-client/assets/images/food/된장.png'),
-    고추장: require('../../app-client/assets/images/food/고추장.png'),
-    케찹: require('../../app-client/assets/images/food/케찹.png'),
+    된장: require('../assets/images/food/soybean_paste.png'),
+    고추장: require('../assets/images/food/red_pepper_paste.png'),
+    케찹: require('../assets/images/food/ketchup.png'),
 
     // 가공/유제품
-    김치: require('../../app-client/assets/images/food/김치.png'),
-    우유: require('../../app-client/assets/images/food/우유.png'),
-    옥수수캔: require('../../app-client/assets/images/food/옥수수캔.png'),
-    캔: require('../../app-client/assets/images/food/캔.png'),
-    치즈: require('../../app-client/assets/images/food/치즈.png'),
+    김치: require('../assets/images/food/kimchi.png'),
+    우유: require('../assets/images/food/milk.png'),
+    옥수수캔: require('../assets/images/food/corn_can.png'),
+    캔: require('../assets/images/food/can.png'),
+    치즈: require('../assets/images/food/cheese.png'),
 }
 
 const Assets = {
